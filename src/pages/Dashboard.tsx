da9fac86edb2d6f645e2b0f4a59501c08fb6e3d6
Dabeer Farooqi <dabeer445@gmail.com>
2024-06-20 20:43:41 +0500

    updated env

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,24 +14,49 @@ import {
 	getStoredCredentials,
 } from '../services/storage';
 
+import { createClient } from '@supabase/supabase-js';
+import { OpenAI } from 'openai';
+
 export interface ConversationWithMessages extends Conversation {
 	messages: Message[];
-	nextMessagesToken?: string;
+	name: string;
+	number: string;
+	threadID: string;
 }
+// export interface ConversationWithMessages extends Conversation {
+// 	messages: Message[];
+// 	nextMessagesToken?: string;
+// }
+
+const openai = new OpenAI({apiKey: import.meta.env.OPENAI_API, dangerouslyAllowBrowser: true});
+  
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const loadConvs = async () => {
+  const { data, error } = await supabase
+    .from('Conversations')
+    .select('*');
+  if (error) throw error;
+  return data;
+};
+
+const fetchMessagesFromOpenAI = async (threadID:string) => {
+  const threadMessages = await openai.beta.threads.messages.list(threadID);
+  return threadMessages.data.map((i)=>( i.content[0]?.text?.value));
+};
+
 
 export const Dashboard = () => {
 	const [isLoadingConversations, setIsLoadingConversations] =
 		useState<boolean>(true);
 
-	const [botInfo, setBotInfo] = useState<{
-		id?: string;
-		name?: string;
-	}>({});
 	const [selectedConversation, setSelectedConversation] =
 		useState<ConversationWithMessages>();
 
 	const [conversationList, setConversationList] = useState<
-		ConversationWithMessages[]
+	ConversationWithMessages[]
 	>([]);
 	const [nextConversationsToken, setNextConversationsToken] =
 		useState<string>();
@@ -45,110 +70,128 @@ export const Dashboard = () => {
 		// window.location.reload();
 	}
 
-	useEffect(() => {
-		if (!botpressClient) {
+	// useEffect(() => {
+	// 	if (!botpressClient) {
+	// 		try {
+	// 			const credentials = getStoredCredentials();
+
+	// 			// if there are credentials saved in the Local Storage, decrypts and creates the client
+	// 			if (credentials) {
+	// 				createClient(
+	// 					credentials.token,
+	// 					credentials.workspaceId,
+	// 					credentials.botId
+	// 				);
+
+	// 				setBotInfo({
+	// 					id: credentials.botId,
+	// 					name: 'Loading',
+	// 				});
+	// 			}
+	// 		} catch (error: any) {
+	// 			toast("Couldn't start the app");
+
+	// 			toast.error(error.message || error);
+	// 		}
+	// 	} else {
+	// 		// if there are conversations already, returns
+	// 		if (conversationList.length > 0) {
+	// 			console.log("There's already conversations loaded");
+	// 			return;
+	// 		} else {
+	// 			console.log("There's no conversations loaded");
+	// 		}
+
+	// 		// if there is a client, loads the conversations
+	// 		(async () => {
+	// 			try {
+	// 				setIsLoadingConversations(true);
+
+	// 				console.log('LOADING CONVERSATIONS');
+
+	// 				const getConversations =
+	// 					await listConversationsWithMessages(
+	// 						botpressClient,
+	// 						undefined,
+	// 						true
+	// 					);
+
+	// 				setConversationList(
+	// 					getConversations.conversations as ConversationWithMessages[]
+	// 				);
+
+	// 				setNextConversationsToken(
+	// 					getConversations.nextConversationsToken
+	// 				);
+	// 			} catch (error: any) {
+	// 				console.log(
+	// 					'ERROR ON GETTING CONVERSATIONS : ',
+	// 					JSON.stringify(error)
+	// 				);
+
+	// 				if (error.code === 429) {
+	// 					toast(
+	// 						'You have reached the limit of requests to the Botpress API... Please try again later'
+	// 					);
+	// 				}
+
+	// 				toast.error("Couldn't load older conversations");
+	// 			} finally {
+	// 				setIsLoadingConversations(false);
+	// 			}
+
+	// 			// const retriable = await pRetry( (func) => {
+	// 			// 	try {
+	// 			// 		func
+	// 			// 	} catch (error) {
+	// 			// 		if (error.code !== 429)
+	// 			// 		{
+	// 			// 			throw new AbortError
+	// 			// 		}
+	// 			// 		throw error
+	// 			// 	}
+	// 			// },{onFailedAttempt: (error) => {
+	// 			// 	throw error
+	// 			// },retries:5})
+
+	// 			try {
+	// 				// tries to get the bot name
+	// 				if (botInfo.id) {
+	// 					const getBot = await botpressClient.getBot({
+	// 						id: botInfo.id,
+	// 					});
+
+	// 					setBotInfo((prev) => ({
+	// 						...prev,
+	// 						name: getBot.bot.name,
+	// 					}));
+	// 				}
+	// 			} catch (error) {
+	// 				console.log(JSON.stringify(error));
+
+	// 				toast.error("Couldn't load bot info");
+	// 			}
+	// 		})();
+	// 	}
+	// }, [botpressClient]);
+	useEffect(()=>{
+		(async () => {
 			try {
-				const credentials = getStoredCredentials();
-
-				// if there are credentials saved in the Local Storage, decrypts and creates the client
-				if (credentials) {
-					createClient(
-						credentials.token,
-						credentials.workspaceId,
-						credentials.botId
-					);
-
-					setBotInfo({
-						id: credentials.botId,
-						name: 'Loading',
-					});
-				}
+			  setIsLoadingConversations(true);
+	
+			  const conversations = await loadConvs();
+			  const messages = await fetchMessagesFromOpenAI(threadID[0].threadID);
+				console.log("MSDS",messages)
+			  setConversationList([{ ...threadID, messages }]);
+			  console.log("CL",conversationList)
 			} catch (error: any) {
-				toast("Couldn't start the app");
-
-				toast.error(error.message || error);
-			}
-		} else {
-			// if there are conversations already, returns
-			if (conversationList.length > 0) {
-				console.log("There's already conversations loaded");
-				return;
-			} else {
-				console.log("There's no conversations loaded");
+			  toast.error("Couldn't load conversations");
+			} finally {
+			  setIsLoadingConversations(false);
 			}
 
-			// if there is a client, loads the conversations
-			(async () => {
-				try {
-					setIsLoadingConversations(true);
-
-					console.log('LOADING CONVERSATIONS');
-
-					const getConversations =
-						await listConversationsWithMessages(
-							botpressClient,
-							undefined,
-							true
-						);
-
-					setConversationList(
-						getConversations.conversations as ConversationWithMessages[]
-					);
-
-					setNextConversationsToken(
-						getConversations.nextConversationsToken
-					);
-				} catch (error: any) {
-					console.log(
-						'ERROR ON GETTING CONVERSATIONS : ',
-						JSON.stringify(error)
-					);
-
-					if (error.code === 429) {
-						toast(
-							'You have reached the limit of requests to the Botpress API... Please try again later'
-						);
-					}
-
-					toast.error("Couldn't load older conversations");
-				} finally {
-					setIsLoadingConversations(false);
-				}
-
-				// const retriable = await pRetry( (func) => {
-				// 	try {
-				// 		func
-				// 	} catch (error) {
-				// 		if (error.code !== 429)
-				// 		{
-				// 			throw new AbortError
-				// 		}
-				// 		throw error
-				// 	}
-				// },{onFailedAttempt: (error) => {
-				// 	throw error
-				// },retries:5})
-
-				try {
-					// tries to get the bot name
-					if (botInfo.id) {
-						const getBot = await botpressClient.getBot({
-							id: botInfo.id,
-						});
-
-						setBotInfo((prev) => ({
-							...prev,
-							name: getBot.bot.name,
-						}));
-					}
-				} catch (error) {
-					console.log(JSON.stringify(error));
-
-					toast.error("Couldn't load bot info");
-				}
-			})();
-		}
-	}, [botpressClient]);
+		  })();
+	},[supabase]);
 
 	async function loadOlderConversations() {
 		if (!botpressClient) {
@@ -191,7 +234,8 @@ export const Dashboard = () => {
 			{/* HEADER */}
 			<Header
 				handleLogout={clearsCredentialsAndClient}
-				botName={botInfo.name}
+				botName="Test"
+				// botName={botInfo.name}
 				className="flex-shrink-0 h-14"
 			/>
 			{/* CONVERSATIONS */}
