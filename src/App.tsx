0b055e4f902d9220351d58c4fa433c5122a50acb
Dabeer Farooqi <dabeer445@gmail.com>
2024-07-11 13:16:55 +0500

    updated build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { BotpressClientContextProvider } from './hooks/botpressClient';
-import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard';
-import { Toaster } from 'react-hot-toast';
 import './styles/tailwind-input.css';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
 import { LoginPage } from './components/LoginPage';
 
 export default function App() {
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,63 +1,63 @@
-import toast from 'react-hot-toast';
-import { useContext, useState } from 'react';
-import { DashboardContext } from '../utils';
+// import toast from 'react-hot-toast';
+// import { useContext, useState } from 'react';
+// import { DashboardContext } from '../utils';
 import { SignInButton } from '@clerk/clerk-react';
 
-const USER_CREDS = {
-	username: "tester@gmail.com",
-	password: "tester12345"
-}
+// const USER_CREDS = {
+// 	username: "tester@gmail.com",
+// 	password: "tester12345"
+// }
 
 export function LoginPage() {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
+	// const [username, setUsername] = useState('');
+	// const [password, setPassword] = useState('');
 
-	const setIsLoggedIn = useContext(DashboardContext)?.setIsLoggedIn
+	// const setIsLoggedIn = useContext(DashboardContext)?.setIsLoggedIn
 
-	function handleSubmitCredentials() {
-		if (!username.length || !password.length) {
-			toast.error('Please inform all the credentials');
-			return;
-		}
-		// if (!token || !url) {
-		// 	toast.error('Please inform all the credentials');
-		// 	return;
-		// }
+	// function handleSubmitCredentials() {
+	// 	if (!username.length || !password.length) {
+	// 		toast.error('Please inform all the credentials');
+	// 		return;
+	// 	}
+	// 	// if (!token || !url) {
+	// 	// 	toast.error('Please inform all the credentials');
+	// 	// 	return;
+	// 	// }
 
-		try {
-			// const splittedURL = url.split('/');
-			// const workspaceId = splittedURL[4];
-			// const botId = splittedURL[6];
+	// 	try {
+	// 		// const splittedURL = url.split('/');
+	// 		// const workspaceId = splittedURL[4];
+	// 		// const botId = splittedURL[6];
 
-			// if (!workspaceId || !botId) {
-			// 	throw new Error();
-			// }
+	// 		// if (!workspaceId || !botId) {
+	// 		// 	throw new Error();
+	// 		// }
 
-			// const bpClient = createClient(token, workspaceId, botId);
+	// 		// const bpClient = createClient(token, workspaceId, botId);
 
-			// if (!bpClient) {
-			// 	throw new Error();
-			// }
+	// 		// if (!bpClient) {
+	// 		// 	throw new Error();
+	// 		// }
 
-			// // saves the encrypted credentials to storage
-			// storeCredentials({ token, workspaceId, botId });			
+	// 		// // saves the encrypted credentials to storage
+	// 		// storeCredentials({ token, workspaceId, botId });			
 
-			if (username === USER_CREDS.username && password === USER_CREDS.password) {
-				setIsLoggedIn?.(true)
-			} else {
-				throw 'Incorrect Credentials'
-			}
-		} catch (error) {
-			toast.error('You have informed invalid credentials');
+	// 		if (username === USER_CREDS.username && password === USER_CREDS.password) {
+	// 			setIsLoggedIn?.(true)
+	// 		} else {
+	// 			throw 'Incorrect Credentials'
+	// 		}
+	// 	} catch (error) {
+	// 		toast.error('You have informed invalid credentials');
 
-			// clearsCredentialsAndClient();
-		}
+	// 		// clearsCredentialsAndClient();
+	// 	}
 
-		setUsername('');
-		setPassword('');
-		// setUserBotpressToken('');
-		// setUserBotpressURL('');
-	}
+	// 	setUsername('');
+	// 	setPassword('');
+	// 	// setUserBotpressToken('');
+	// 	// setUserBotpressURL('');
+	// }
 
 	return (
 		<div className="flex flex-col h-screen">
